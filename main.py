d6ff45aef9630146577c1db44db87c171c42aa8c
hf1337 <hf1337@bk.ru>
2025-03-27 15:35:16 +0300

    Добавить пункт 3.5 "Переменные и конкатенация"
    * README.md: добавить пункт 3.5 "Переменные и конкатенация", улучшить читаемость и обновить содержание
    
    * main.py: добавить пример демонстрационного задания

--- main.py
+++ main.py
@@ -5,14 +5,7 @@
 Любое коммерческое использование запрещено.
 """
 
-euros_count = 100
+first_name = "Johnny"
+greeting = "Hello"
 
-# Евро в долларах
-dollar_count = 100 * 1.25
-print(dollar_count)
-
-# Доллары в юанях
-yuans_count = dollar_count * 6.91
-print(yuans_count)
-
-print("Euros: " + str(euros_count) + " " + "Dollars: " + str(dollar_count) + " " + "Yuans: " + str(yuans_count))
+print(greeting + ", " + first_name + "!")
