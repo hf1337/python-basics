366c6b538caf86d745310fdf434df0a1bd65ae7a
hf1337 <hf1337@bk.ru>
2025-04-03 03:07:16 +0300

    Добавить раздел 5. "Типы данных" и пункт 5.1 "Примитивные типы данных"
    * README.md: добавить раздел 5. "Типы данных", пункт 5.1 "Примитивные типы данных" и обновить содержание
    
    * main.py: добавить пример демонстрационного задания

--- main.py
+++ main.py
@@ -5,7 +5,17 @@
 Любое коммерческое использование запрещено.
 """
 
-# Пример multi-line строки
-text = """But it was not an easy way, for what Fion's father did not know
-in arms could not be taught to him even by Morna. Still the hound that can wait
-will catch a hair at last and even Banan sleeps."""
+# Строка
+print("Hello, World!")  # => Hello, World!
+
+# Целое число
+print(7)  # => 7
+
+# Рациональное число
+print(7.77)  # => 7.77
+
+# Логический тип
+has_error = True
+
+# Тип None
+result = None
